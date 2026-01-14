fe6b1cbfa54de866227d17bde146183039512432
Arjun Krishnatry <akrish73@uwo.ca>
2026-01-14 12:49:04 -0500

    fixed filepath errors

--- data_splits.py
+++ data_splits.py
@@ -12,22 +12,21 @@ print(df.head(5))
 
 # load the sample generated training data from csv file
 gen_csvDataPath = "csv_data/generated_pairs_train.csv"
-if not Path(csvDataPath).exists():
-    raise FileNotFoundError(f"CSV data file not found: {csvDataPath}")
-gen_df = pd.read_csv(csvDataPath)
+if not Path(gen_csvDataPath).exists():
+    raise FileNotFoundError(f"CSV data file not found: {gen_csvDataPath}")
+gen_df = pd.read_csv(gen_csvDataPath)
 print('Loaded generated df shape:', gen_df.shape)
 print(gen_df.head(5))
 
 
-# select only the relevant columns
+# select only the relevant columns from the training csv
 selected_cols = ['code1', 'code2', 'similar']
 df = df[selected_cols]
 print(df.head(5))
 
-# select only the relevant columns
-selected_cols = ['code1', 'code2', 'similar']
-gen_df = gen_df[selected_cols]
-print(df.head(5))
+# added labels to generated training data which allows it to be merged with sample pairs
+gen_df.columns = selected_cols
+print(gen_df.head(5))
 
 # Merge the generated pairs and sample pairs
 df = pd.concat([df, gen_df], axis=0)
@@ -66,16 +65,17 @@ test_df = pd.read_csv(csvTestDataPath)
 
 # get the generated test data from csv file
 gen_csvTestDataPath = "csv_data/generated_pairs_test.csv"
-if not Path(csvTestDataPath).exists():
-    raise FileNotFoundError(f"CSV test data file not found: {csvTestDataPath}")
+if not Path(gen_csvTestDataPath).exists():
+    raise FileNotFoundError(f"CSV test data file not found: {gen_csvTestDataPath}")
 gen_test_df = pd.read_csv(gen_csvTestDataPath)
 
-# select columns for generated and selected pairs present
+# select columns for selected pairs present
 selected_test_cols = ['code1', 'code2']
 gen_test_df = gen_test_df[selected_test_cols]
 print('\nGenerated Test data shape:', gen_test_df.shape)
 print(test_df.head(5))
 
+# Making sure that the columns for generated pairs are correct
 test_df = test_df[selected_test_cols]
 print('\nTest data shape:', test_df.shape)
 print(test_df.head(5))
