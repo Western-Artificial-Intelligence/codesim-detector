17dd2950afbbf45380db5ead7314ad7becd0474f
Arjun Krishnatry <akrish73@uwo.ca>
2026-01-14 00:46:15 -0500

    updated to turn generated pairs into parquet

--- data_splits.py
+++ data_splits.py
@@ -10,12 +10,29 @@ df = pd.read_csv(csvDataPath)
 print('Loaded df shape:', df.shape)
 print(df.head(5))
 
+# load the sample generated training data from csv file
+gen_csvDataPath = "csv_data/generated_pairs_train.csv"
+if not Path(csvDataPath).exists():
+    raise FileNotFoundError(f"CSV data file not found: {csvDataPath}")
+gen_df = pd.read_csv(csvDataPath)
+print('Loaded generated df shape:', gen_df.shape)
+print(gen_df.head(5))
+
 
 # select only the relevant columns
 selected_cols = ['code1', 'code2', 'similar']
 df = df[selected_cols]
 print(df.head(5))
 
+# select only the relevant columns
+selected_cols = ['code1', 'code2', 'similar']
+gen_df = gen_df[selected_cols]
+print(df.head(5))
+
+# Merge the generated pairs and sample pairs
+df = pd.concat([df, gen_df], axis=0)
+print('Final training df shape:', df.shape)
+print(df.head(5))
 
 # save the sample data to parquet
 Path("data").mkdir(parents=True, exist_ok=True)
@@ -47,13 +64,26 @@ if not Path(csvTestDataPath).exists():
     raise FileNotFoundError(f"CSV test data file not found: {csvTestDataPath}")
 test_df = pd.read_csv(csvTestDataPath)
 
+# get the generated test data from csv file
+gen_csvTestDataPath = "csv_data/generated_pairs_test.csv"
+if not Path(csvTestDataPath).exists():
+    raise FileNotFoundError(f"CSV test data file not found: {csvTestDataPath}")
+gen_test_df = pd.read_csv(gen_csvTestDataPath)
 
-# select columns if present
+# select columns for generated and selected pairs present
 selected_test_cols = ['code1', 'code2']
+gen_test_df = gen_test_df[selected_test_cols]
+print('\nGenerated Test data shape:', gen_test_df.shape)
+print(test_df.head(5))
+
 test_df = test_df[selected_test_cols]
 print('\nTest data shape:', test_df.shape)
 print(test_df.head(5))
 
+# Merge the generated pairs and sample pairs
+test_df = pd.concat([test_df, gen_test_df], axis=0)
+print('\nFinal test data shape:', test_df.shape)
+print(test_df.head(5))
 
 # save test parquet
 testParquetFilePath = "data/test.parquet"
